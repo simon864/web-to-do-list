25c800befe011b7351684a07b73130cea5296113
simon864 <121446009+simon864@users.noreply.github.com>
2024-10-12 12:43:54 +0700

    add creating tasks, there are shortcomings

--- src/scripts/add-task.js
+++ src/scripts/add-task.js
@@ -2,27 +2,68 @@ const tasksContainer = document.querySelector(".tasks-container");
 const addTuskButton = document.getElementById("add-task-button");
 const inputTitle = document.getElementById("input-title");
 const inputAbout = document.getElementById("input-about");
+const zeroTaskContainer = document.querySelector(".zero-task-container");
 
-addTuskButton.onclick = function () {
-  document.createElement("div");
-  const title = inputTitle.value;
-  const about = inputAbout.value;
+function getTask() {
+  const taskContainer = document.createElement("div");
+  taskContainer.classList.add("task-container");
 
-  if (title && about) {
-    const task = document.createElement("div");
-    task.classList.add("task");
-    task.innerHTML = `<div class="text-task-container"> <h2> ${title} </h2>
-    <p> ${about} </p> </div>
-    <div class = delete-task-button <button>
-    <img src="src/assets/images/cross-button.svg" alt=""> </button> </div>`;
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("text-container");
 
-    const closeButton = task.querySelector(".delete-task-button");
-    closeButton.addEventListener("click", () => {
-      tasksContainer.removeChild(task);
-    });
+  const title = document.createElement("h2");
+  title.textContent = inputTitle.value;
+  textContainer.appendChild(title);
 
-    tasksContainer.appendChild(task);
-  } else {
-    alert("Please enter both title and about");
+  const about = document.createElement("p");
+  about.textContent = inputAbout.value;
+  textContainer.appendChild(about);
+
+  taskContainer.appendChild(textContainer);
+
+  const deleteButtonContainer = document.createElement("div");
+  deleteButtonContainer.classList.add("delete-button-container");
+
+  const deleteTaskButton = document.createElement("button");
+  deleteTaskButton.classList.add("delete-task-button");
+  deleteButtonContainer.appendChild(deleteTaskButton);
+
+  const imageCross = document.createElement("img");
+  imageCross.src = "src/assets/images/cross-button.svg";
+  deleteTaskButton.appendChild(imageCross);
+
+  taskContainer.appendChild(deleteButtonContainer);
+
+  tasksContainer.appendChild(taskContainer);
+
+  zeroTaskContainer.style.display = "none";
+}
+
+function checkTask() {
+  if (tasksContainer.children.length === 0) {
+    zeroTaskContainer.style.display = "flex";
   }
-};
+}
+
+addTuskButton.addEventListener("click", () => {
+  if (inputTitle.value !== "") {
+    checkTask();
+    getTask();
+    inputTitle.value = "";
+    inputAbout.value = "";
+  } else {
+    alert("Введите заголовок!");
+  }
+});
+
+tasksContainer.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-task-button")) {
+    const taskContainer = event.target.parentElement.parentElement;
+    deleteTask(taskContainer);
+    checkTask();
+  }
+});
+
+function deleteTask(taskContainer) {
+  taskContainer.remove();
+}
